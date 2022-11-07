5065a7596b04879704784ce7cef3a721eafc1819
seanf316 <seanfinn316@gmail.com>
2022-11-07 21:53:47 +0000

    Added stock values

--- run.py
+++ run.py
@@ -110,7 +110,12 @@ def calculate_stock_data(data):
         
     return new_stock_data
         
-        
+
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+    new_values = {key: value for key, value in zip(headings, data)}
+    return f"Make the following number of sandwiches for next Market:\n {new_values}"
+
 def main():
     """
     Run all program functions
@@ -119,13 +124,12 @@ def main():
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
-    print(stock_data)
     update_worksheet(stock_data, "stock")
-
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 print("Welcome to Love Sandwiches Data Automation\n") 
 main()
